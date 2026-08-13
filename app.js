5eeb7c4fa90179cf92a1a0589ae151763b4f50c2
Cadet <cadet@example.com>
2026-08-13 11:11:33 +0530

    style: use icon only for home breadcrumb

--- app.js
+++ app.js
@@ -620,7 +620,7 @@ window.updateBreadcrumbs = function() {
 
     if (index === 0 && item.label === 'Home') {
 
-      element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right: 4px;"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg> ${item.label}`;
+      element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: middle;"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>`;
 
     } else {
 
